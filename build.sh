e4f97381ad716501944cbe1fba474aba34722e8a
Andrew Turner <andrew@fubar.geek.nz>
2016-04-16 18:18:05 +0100

    Add the Foundation model DTS and a build sript
    These will be to run FreeBSD under the Foundation Models.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ROOT=`dirname $0`
+
+CPPARGS="-P -x assembler-with-cpp"
+CPPARGS="${CPPARGS} -I ${ROOT}/dts"
+CPPARGS="${CPPARGS} -include ${ROOT}/dts/foundation-v8-gicv3.dts /dev/null"
+
+cpp ${CPPARGS} | dtc -o fvp-foundation-gicv3-psci.dtb -
+
