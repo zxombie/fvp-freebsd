9b2574bd71cab6b6536f58d29e3a339debfde5bc
Andrew Turner <andrew@fubar.geek.nz>
2016-04-16 18:49:06 +0100

    Switch to the GICv2 otherwise FreeBSD fails to boot

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ ROOT=`dirname $0`
 
 CPPARGS="-P -x assembler-with-cpp"
 CPPARGS="${CPPARGS} -I ${ROOT}/dts"
-CPPARGS="${CPPARGS} -include ${ROOT}/dts/foundation-v8-gicv3.dts /dev/null"
+#CPPARGS="${CPPARGS} -include ${ROOT}/dts/foundation-v8-gicv3.dts /dev/null"
+CPPARGS="${CPPARGS} -include ${ROOT}/dts/foundation-v8-gicv2.dts /dev/null"
 
 cpp ${CPPARGS} | dtc -O dtb -o fvp-foundation-gicv3-psci.dtb -
 
